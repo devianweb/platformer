0814e275a9aa6942aa9970f40ed489689c033dba
carrcig <87659522+carrcig@users.noreply.github.com>
2023-12-27 01:30:01 +0000

    clean up

--- src/main/java/core/Game.java
+++ src/main/java/core/Game.java
@@ -25,13 +25,12 @@ public class Game implements Runnable {
     public void run() {
         double timePerFrame = 1000000000.0 / FPS_SET;
         long lastFrame = System.nanoTime();
-        long now = System.nanoTime();
 
         int frames = 0;
         long lastCheck = System.currentTimeMillis();
 
         while (true) {
-            now = System.nanoTime();
+            var now = System.nanoTime();
             if (now - lastFrame >= timePerFrame) {
                 gamePanel.repaint();
                 lastFrame = now;
